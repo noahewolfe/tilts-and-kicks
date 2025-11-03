1c5d800fd83dde9f53968b98aac87d6fe1405345
Noah Wolfe <newolfe@mit.edu>
2025-11-03 09:27:11 -0500

    add requeue flag to allow requeueing of pe script

--- code/pe.sh
+++ code/pe.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 #SBATCH --job-name=pe
-#SBATCH --partition=submit
-#SBATCH --time=01-00:00
+#SBATCH --partition=mit_normal
+#SBATCH --time=00-12:00
 #SBATCH --mem=4GB
-#SBATCH -c 1
-#SBATCH --array=0-999
+#SBATCH -c 1 
+#SBATCH --array=0-99
+#SBATCH --requeue
 
-outdir="/work/submit/newolfe/tilts-and-kicks/data/pe/snr15/ct-iso"
+outdir="../data/pe/snr15/ct-0p9"
 outdir="${outdir}/${SLURM_ARRAY_TASK_ID}"
 
 echo $outdir
 mkdir -p $outdir
 
-py="/work/submit/newolfe/miniforge3/envs/just-for-kicks/bin/python"
-timeout 18h $py pe.py \
+#py="/work/submit/newolfe/miniforge3/envs/just-for-kicks/bin/python"
+py="/home/newolfe/.conda/envs/just-for-kicks/bin/python"
+timeout 8h $py pe.py \
     --outdir $outdir \
     --npool 1 \
     --prior-path ./priors/bbh.prior \
     --event-index $SLURM_ARRAY_TASK_ID \
     --time-reference H1 \
     --nlive 1000 \
-    --catalog-path /work/submit/newolfe/tilts-and-kicks/data/vt/snr15/ct-iso/detectable.hdf5 \
+    --catalog-path ../data/vt/snr15/ct-0p9/detectable.hdf5 \
     >> "${outdir}/log.out" 2>> "${outdir}/log.err"
 
 if [[ $? == 124 ]]; then 
