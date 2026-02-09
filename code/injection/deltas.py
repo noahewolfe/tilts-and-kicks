d1533a29de986fc541a197bbf9e6fda4f0e13c47
Noah Wolfe <noah.wolfe@ligo.org>
2026-02-08 18:00:23 -0800

    pass through snr cut when testing with deltas

--- code/injection/deltas.py
+++ code/injection/deltas.py
@@ -26,21 +26,23 @@ from models import log_iid_spin_mag_truncnorm
 from models import log_nid_iso_gauss_tilt
 from models import BrokenPowerlawPlusTwoPeaks_PrimaryMass_FullSmooth
 
-nobs = 150
-maximum_variance = 1
+nobs = 350
+maximum_variance = 5 
+outdir = '../../data/runs/tests/260207-n350-rho15'
 
 parser = ArgumentParser()
 parser.add_argument('--path', help='path to vt file')
 parser.add_argument('--truths', help='path to json file with true parameters')
-
+parser.add_argument('--cut', default=11, type=int)
 
 def parse_args():
     """ return command-line arguments """
+    os.makedirs(outdir, exist_ok=True)
     args = parser.parse_args()
     with open(args.truths, 'r') as f:
         truths = json.loads(f.read())
         truths = {k : np.asarray(v).item() for k, v in truths.items()}
-    return args.path, truths
+    return args.path, truths, args.cut
 
 
 def log_model(dataset, parameters):
@@ -86,19 +88,24 @@ def log_model(dataset, parameters):
     return log_p_m1 + log_p_q + log_p_z + log_p_chi + log_p_tau
 
 
-def get_data(key, nobs, path):
+def get_data(key, nobs, path, cut=11):
     """ load in detected injections; format for pdet estimation and delta
         posteriors.
     """
     detectable = h5ify.load(path)
+    print(f'using snr threshold {cut}')
+
+    mask = detectable['network_matched_filter_snr'] > cut
+    for k in list(detectable.keys()):
+        if k not in ['attrs', 'total_generated', 'model', 'parameters']:
+            detectable[k] = detectable[k][mask]
+
     ndet = len(detectable['log_prior'])
 
     print('ndet =', ndet)
     print('ndet / ntot =', ndet / detectable['total_generated'])
 
     detectable['mass_1'] = detectable.pop('mass_1_source')
-
-    #print(detectable['parameters'])
 
     idxs = jax.random.choice(
         key,
@@ -115,6 +122,14 @@ def get_data(key, nobs, path):
     posteriors.pop('parameters')
     posteriors = {k : v[idxs].reshape(nobs, 1) for k, v in posteriors.items()}
 
+    if os.path.isfile(f'{outdir}/posteriors.h5'):
+        p = h5ify.load(f'{outdir}/posteriors.h5')
+        for k, v in posteriors.items():
+            assert np.all(v == p[k])        
+#assert posteriors == p
+    else:
+        h5ify.save(f'{outdir}/posteriors.h5', posteriors)
+
     return posteriors, detectable
 
 
@@ -124,12 +139,12 @@ def taper(v):
 
 
 if __name__ == '__main__':
-    path, truths = parse_args()
+    path, truths, cut = parse_args()
     truths['log_sigma_spin'] = np.log(truths.pop('sigma_spin')).item()
     truths['log_mu_spin'] = np.log(truths.pop('mu_spin')).item()
     print(truths)
 
-    posteriors, injections = get_data(jax.random.key(42), nobs, path)
+    posteriors, injections = get_data(jax.random.key(42), nobs, path, cut=cut)
 
     likelihood = get_bilby_likelihood(
         log_model,
@@ -154,8 +169,6 @@ if __name__ == '__main__':
         minimum=np.log(0.9).item(),
         maximum=0.0
     )
-
-    outdir = '../../data/runs/tests/260207'
 
     result = run_sampler(
         likelihood=likelihood,
