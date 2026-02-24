fix sign error when using expit

code/strong/models.py
@@ -439,7 +439,7 @@ def stegmann_spin_components(
             0.5 * 0.5 
 
     # Mass-dependent transition function
-    zeta = jax.scipy.special.expit(m_cut - m_1)   
+    zeta = jax.scipy.special.expit(m_1 - m_cut)   
  
     # Combine components with mass-dependent transition
     return zeta, comp1, comp2, comp3

code/strong/multimass-gwpop.py
@@ -277,7 +277,7 @@ def components_and_weights(parameters):
 
     mm = xp.linspace(3, 300, 500)
     dataset = dict(mass_1=mm)
-    zeta = jax.scipy.special.expit(m_cut - mm)
+    zeta = jax.scipy.special.expit(mm - m_cut)
 
     weight = (1 - zeta) * weight_a
     weight_iso = (1 - zeta) * (1 - weight_a)