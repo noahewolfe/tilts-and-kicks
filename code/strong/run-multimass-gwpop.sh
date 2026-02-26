dfa0081f98efcd9938d7a8528a31fc8bdb50a8fd
Noah Wolfe <noah.wolfe@ligo.org>
2026-02-26 12:03:14 -0800

    working on reparam of mixing models

--- code/strong/run-multimass-gwpop.sh
+++ code/strong/run-multimass-gwpop.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.75
+export CUDA_VISIBLE_DEVICES=1
 export JAX_ENABLE_X64=1
 
-data="stegmann"
-model="default-spin-simple-power-law-mass"
-#model="default-spin-bpl2p-mass"
+data="noah"
+#model="default-spin-simple-power-law-mass"
+model="default-spin-bpl2p-mass"
 #model="twomass"
 #model="threemass"
 nlive=1000
@@ -23,6 +24,7 @@ $HOME/.conda/envs/just-for-kicks/bin/python -u multimass-gwpop.py \
     --maximum-uncertainty $var \
     --nlive $nlive \
     --stable-expit \
+    --priors ./priors/onemass-only-mass.prior \
     > $outdir/log.out 2> $outdir/log.err
 
 status=$?
